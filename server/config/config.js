9037c39c616f09157df906e2697893f4396e42f1
Fran <rivilla13@gmail.com>
2020-05-22 20:12:18 +0200

    Variable de entorno Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -16,8 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev' && false) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    //urlDB = 'mongodb+srv://bsn_admin:Pwy5uh7vf34RaFZl@cluster0-viu0r.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
-    urlDB = 'mongodb+srv://bsn_admin:Pwy5uh7vf34RaFZl@cluster0-viu0r.mongodb.net/cafe?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+    urlDB = process.env.MONGO_URI;
 }
 // Guardamos la urlDB en una variable nueva que creamos para ello
 process.env.URLDB = urlDB;
